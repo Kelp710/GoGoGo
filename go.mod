ae4320e7fb8b7406609573825bb1cc5a805b2d57
kelp710 <bz176354@gmail.com>
2023-06-13 14:24:13 +0900

    Http Get Put request practice

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Kelp710/GoGoGo
 
 go 1.20
+
+require golang.org/x/tour v0.1.0
--- test.go
+++ test.go
@@ -1,12 +1,22 @@
 package main
 
 import (
-	"fmt"
-
-	app "github.com/Kelp710/GoGoGo/layer1"
+	"golang.org/x/tour/pic"
 )
 
+func Pic(dx, dy int) [][]uint8 {
+	pic := make([][]uint8, dy)
+
+	for y := range pic {
+
+		pic[y] = make([]uint8, dx)
+		for x := range pic[y] {
+			pic[y][x] = uint8((x * y))
+		}
+	}
+	return pic
+}
+
 func main() {
-	fmt.Println("Hello World")
-	app.DogCirclingAround()
+	pic.Show(Pic)
 }
